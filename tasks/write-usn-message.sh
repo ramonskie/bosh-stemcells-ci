5065c1dc4b4555e13bed5cde373f315e19a43641
Aakash Shah <ashah@pivotal.io>
2019-06-19 11:30:36 -0700

    Strip quotes from jq output

--- tasks/write-usn-message.sh
+++ tasks/write-usn-message.sh
@@ -5,19 +5,19 @@ git clone bosh-linux-stemcell-builder bosh-linux-stemcell-builder-out
 usn_json="${PWD}/usn-source/usn.json"
 
 commit_body=$(cat <<EOF
-url: $(jq .url "${usn_json}")
-priorities: $(jq '.priorities | join(",")' "${usn_json}")
-description: $(jq .description "${usn_json}")
+url: $(jq -r .url "${usn_json}")
+priorities: $(jq -r '.priorities | join(",")' "${usn_json}")
+description: $(jq -r .description "${usn_json}")
 cves:
-  $(jq '.cves[] | "* \(.)"' "${usn_json}")
+  $(jq -r '.cves[] | "* \(.)"' "${usn_json}")
 EOF
 )
 
 pushd bosh-linux-stemcell-builder-out
-	git add -A
-	git config --global user.email "ci@localhost"
-	git config --global user.name "CI Bot"
-	git commit --allow-empty -m "$(jq .title "${usn_json}")" -m "${commit_body}"
+  git add -A
+  git config --global user.email "ci@localhost"
+  git config --global user.name "CI Bot"
+  git commit --allow-empty -m "$(jq -r .title "${usn_json}")" -m "${commit_body}"
 popd
 
 exit 1
